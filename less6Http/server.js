070268dba311a4f34f433727c75075ee49df6871
Piv0var <ivlasikhin7@gmail.com>
2024-09-25 12:32:38 +0300

    assdasdasd

--- less6Http/server.js
+++ less6Http/server.js
@@ -1,79 +1,94 @@
-import {log} from "node:console";
+import { log } from "node:console";
 import http from "node:http";
 import url from "node:url";
 import path from "node:path";
 import fs from "node:fs";
 import { fileURLToPath } from "node:url";
+import querystring from "querystring";
+
 const PORT = 3000;
 const mimeTypes = {
-  '.css':"text/css",
-  '.js':"text/javascript",
-  '.png':"image/png",
-  '.jpeg':"image/jpeg",
-  '.jpg':"image/jpg",
+  '.css': "text/css",
+  '.js': "text/javascript",
+  '.png': "image/png",
+  '.jpeg': "image/jpeg",
+  '.jpg': "image/jpg",
 };
 
-const getStaticFile =(res,filePath,ext) => {
+const getStaticFile = (res, filePath, ext) => {
   res.setHeader("Content-Type", mimeTypes[ext]);
-  fs.readFile(path.join(".","public", filePath), (err, data)=>{
-    if(err){
-      res.statusCode=404;
+  fs.readFile(path.join(".", "public", filePath), (err, data) => {
+    if (err) {
+      res.statusCode = 404;
       res.end();
-    } else{
+    } else {
       res.end(data);
     }
   });
-
 };
 
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
 
-const server = http.createServer((req,res)=>{
-  const queryParams = url
-  switch(url){
+  switch (parsedUrl.pathname) {
     case '/':
       res.write(
         fs.readFileSync(
-      path.join(import.meta.dirname,
-        "public", "pages", "index.html")
-      ));
+          path.join(import.meta.dirname, "public", "pages", "index.html")
+        )
+      );
       res.end();
-
       break;
+
     case "/contacts":
       res.write(
         fs.readFileSync(
-      path.join(import.meta.dirname,
-        "public", "pages", "contacts.html")
-      ));
+          path.join(import.meta.dirname, "public", "pages", "contacts.html")
+        )
+      );
       res.end();
-
       break;
-      
-      case "/products":
+
+    case "/login":
+      if (req.method === 'GET') {
+        // Повертаємо сторінку з формою логіну
         res.write(
           fs.readFileSync(
-        path.join(import.meta.dirname,
-          "public", "pages", "form.html")
-        ));
+            path.join(import.meta.dirname, "public", "pages", "login.html")
+          )
+        );
         res.end();
-  
-        break;
-        
-    default:
-      const ext = path.extname(url);
-      if(ext in mimeTypes){
-        getStaticFile(res,url,ext);
+      } else if (req.method === 'POST') {
+        // Обробляємо POST запит з форми
+        let body = '';
+        req.on('data', chunk => {
+          body += chunk.toString();
+        });
+
+        req.on('end', () => {
+          const postData = querystring.parse(body);
+          log(`Username: ${postData.username}, Password: ${postData.password}`);
+
+          // Відповідаємо користувачу
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write('<h1>Авторизація виконана успішно</h1>');
+          res.end();
+        });
       }
-      else{
+      break;
+
+    default:
+      const ext = path.extname(parsedUrl.pathname);
+      if (ext in mimeTypes) {
+        getStaticFile(res, parsedUrl.pathname, ext);
+      } else {
         res.statusCode = 404;
         res.end();
       }
       break;
   }
-  //res.end();
-  // res.write("<div><h1>dsfsffff</h1></div>");
-
 });
-server.listen(3000,() =>{
-  log(`server start`)
-})
+
+server.listen(PORT, () => {
+  log(`Server started on port ${PORT}`);
+});
